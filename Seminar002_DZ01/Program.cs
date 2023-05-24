3984e409921fc4bae4d8dffdeed21eb3418d89ea
Gocha <Frost220v@gmail.com>
2023-05-24 20:59:19 +0500

    принимает на вход трёхзначное число и на выходе показывает вторую цифру

---
+++ Seminar002_DZ01/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 10: Напишите программу, которая принимает на
+//  вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = num/100;
+if(num1==0)
+{
+    Console.Write("Число не является трех значным");  
+}
+else if(num1>0)
+{
+int num2 = num%10;
+int num3 = num1*100+num2;
+int num4 = num-num3;
+int result = num4/10;
+Console.WriteLine($"ваше число между первым и последним {result}");
+}
